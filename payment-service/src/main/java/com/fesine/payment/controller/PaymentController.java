3c23491d96c54da9aad226a35dddadcedc6e8a42
fesine <240944773@qq.com>
2019-11-26 10:31:26 +0800

    Initial commit

---
+++ payment-service/src/main/java/com/fesine/payment/controller/PaymentController.java
@@ -0,0 +1,49 @@
+package com.fesine.payment.controller;
+
+import com.fesine.payment.po.Balance;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @description: 类描述
+ * @author: fesine
+ * @createTime:2019/11/26
+ * @update:修改内容
+ * @author: fesine
+ * @updateTime:2019/11/26
+ */
+@RestController
+public class PaymentController {
+    @Value("${sleep:0}")
+    private int sleep;
+
+    final static Map<Integer, Balance> balanceMap = new HashMap() {
+        {
+            put(1, new Balance(1, 10, 1000));
+            put(2, new Balance(2, 0, 10000));
+            put(3, new Balance(3, 100, 0));
+        }
+    };
+
+    @RequestMapping("/pay/balance/{id}")
+    public Balance getBalance(@PathVariable Integer id) {
+        System.out.println("request: /pay/balance?id=" + id + ", sleep: " + sleep);
+        if (sleep > 0) {
+            try {
+                Thread.sleep(sleep);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        if (id != null && balanceMap.containsKey(id)) {
+            return balanceMap.get(id);
+        }
+        return new Balance(0, 0, 0);
+    }
+
+}
---
+++ payment-service/src/main/java/com/fesine/payment/po/Balance.java
@@ -0,0 +1,62 @@
+package com.fesine.payment.po;
+
+/**
+ * @description: 类描述
+ * @author: fesine
+ * @createTime:2019/11/26
+ * @update:修改内容
+ * @author: fesine
+ * @updateTime:2019/11/26
+ */
+public class Balance {
+    private int id;
+    private int diamond;
+    private int ticket;
+    private String message;
+
+    public Balance() {
+    }
+
+    public Balance(int id, int diamond, int ticket) {
+        this(id, diamond, ticket, "OK");
+    }
+
+    public Balance(int id, int diamond, int ticket, String message) {
+        this.id = id;
+        this.diamond = diamond;
+        this.ticket = ticket;
+        this.message = message;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDiamond() {
+        return diamond;
+    }
+
+    public void setDiamond(int diamond) {
+        this.diamond = diamond;
+    }
+
+    public int getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(int ticket) {
+        this.ticket = ticket;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
